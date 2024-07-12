ae34272023582c629894c1a5f3000f924d04e484
Proximate <marco.rensch@area59.ch>
2024-07-12 09:03:06 +0200

    Added JS Features

---
+++ assets/js/nxd-slider-3d.js
@@ -0,0 +1,94 @@
+class NxdSlider3D {
+    element;
+    items;
+    rotationX;
+    rotationY = 0;
+    perspective;
+
+    constructor(selector) {
+        this.element = document.querySelector(selector);
+        this.items =  Array.from(this.element.querySelectorAll(".item"))
+        this.addItemEventListeners();
+    }
+
+    pauseAnimation() {
+        if (!this.element) return;
+        this.element.style.animationPlayState = "paused";
+    }
+
+    startAnimation() {
+        if (!this.element) return;
+        this.element.style.animationPlayState = "";
+    }
+
+    showLightbox() {
+        const dialog = this.querySelector("dialog");
+        dialog.showModal();
+    }
+
+    addControllerEventListeners() {
+        if(!this.items.length) return;
+        const rotationDegPerStep = 360 / this.items.length;
+        const $sliderElement = this.element.querySelector(".slider");
+        console.log("addControllerEventListeners");
+        const prevBtn = this.element.querySelector('[data-click="prev"]');
+        const nextBtn = this.element.querySelector('[data-click="next"]');
+        console.log(prevBtn)
+        console.log(nextBtn)
+
+        if(prevBtn && nextBtn) {
+            $sliderElement.style.transition = "transform 1s";
+            console.log("all ready for next and prev")
+            prevBtn.addEventListener("click", () => {
+                // Rotate the sliderElement one step back
+                this.rotationY = this.rotationY + rotationDegPerStep;
+                $sliderElement.style.transform = `perspective(${this.perspective}px) rotateX(${this.rotationX}deg) rotateY(${this.rotationY}deg)`;
+            });
+
+            nextBtn.addEventListener("click", () => {
+                // Rotate the sliderElement one step further
+                this.rotationY = this.rotationY - rotationDegPerStep;
+                $sliderElement.style.transform = `perspective(${this.perspective}px) rotateX(${this.rotationX}deg) rotateY(${this.rotationY}deg)`;
+            });
+        }
+    }
+
+    addItemEventListeners() {
+        if (!this.items) return;
+
+        this.items.forEach(item => {
+            item.addEventListener("mouseover", this.pauseAnimation.bind(this));
+            item.addEventListener("mouseout", this.startAnimation.bind(this));
+            item.addEventListener("click", this.showLightbox.bind(item));
+
+            // Dialog Close with btn
+            const closeBtn = item.querySelector(".lightbox-close");
+            closeBtn.addEventListener("click", (event) => {
+                event.stopPropagation();
+                const dialog = closeBtn.closest("dialog");
+                if (typeof dialog.close === "function") {
+                    dialog.close();
+                }
+            });
+            // Dialog close via backdrop:
+            const dialog = item.querySelector("dialog");
+            dialog.addEventListener("click", (event) => {
+                console.log("dialog clicked")
+                event.stopPropagation();
+                if (event.target === dialog && typeof dialog.close === "function") {
+                    dialog.close();
+                }
+            });
+        });
+    }
+
+    setRotationX(val) {
+        this.rotationX = val;
+    }
+
+    setPerspective(val) {
+        this.perspective = val;
+    }
+}
+
+export { NxdSlider3D };
--- index.php
+++ index.php
@@ -1,3 +1,36 @@
+<?php
+
+$extensionID = 21;
+
+$autoRunDirection = ""; // counter-clockwise | clockwise | ""
+$showControls = $autoRunDirection === '';
+$sliderElWidth = "250px";
+$previewRatio = "16:14";
+$rotationX = -20;
+$perspective = 1000;
+
+$previewRatioW = intval(explode(':', $previewRatio)[0]);
+$previewRatioH = intval(explode(':', $previewRatio)[1]);
+$paddingPercent = 100 / $previewRatioW * $previewRatioH;
+
+$cssVars = <<<CSS
+        #module_{$extensionID} {
+                --sliderElWidth: 10vw; /* 200px    */
+                --sliderElHeight: 250px; /* 250px    */
+                --sliderElHPaddingPrecent: {$paddingPercent}%; /* 100% */
+                --sliderTopMargin: 20%;
+                --rotateX: {$rotationX}deg; /* -10deg   */
+                --perspective: {$perspective}px; /* 1000px   */
+                --radius: 30vw; /* 550px    */
+                --duration: 20s; /* 20s      */
+                --modalBackgroundColor: rgba(12, 12, 12, 1);
+                --modalTextColor: #d6d6d6;
+                --modalBorderRadius: .6rem;
+                --previewBorderRadius: .2rem;
+        }
+CSS;
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,23 +39,68 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Crazy 3D Image Slider Carousel</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css?v=1.0.9">
+    <style>
+        <?php echo $cssVars;?>
+    </style>
+    <script type="module">
+        import {NxdSlider3D} from "./assets/js/nxd-slider-3d.js?v=1.0.3";
+
+        document.addEventListener("DOMContentLoaded", () => {
+            const nxd3DSlider = new NxdSlider3D("#module_<?php echo $extensionID; ?>");
+            nxd3DSlider.setRotationX(<?php echo $rotationX;?>);
+            nxd3DSlider.setPerspective(<?php echo $perspective;?>);
+            const autoRunDirection = "<?php echo $autoRunDirection; ?>";
+            const showControls = <?php echo $showControls;?>;
+            if (!autoRunDirection.trim() && showControls) {
+                nxd3DSlider.addControllerEventListeners();
+            }
+        })
+    </script>
 </head>
 <body>
-<div class="banner">
-    <div class="slider counter-clockwise" style="--quantity:6">
-        <?php for ($i = 1; $i <= 6; $i++):?>
-            <div class="item" style="--position:<?php echo $i;?>">
-                <img src="images/example_<?php echo $i;?>.jpg" alt="Example Image <?php echo $i;?>">
+<div class="nxd-slider-container">
+    <div id="module_<?php echo $extensionID; ?>">
+        <div class="banner">
+            <div class="slider-container">
+                <div class="slider <?php echo $autoRunDirection; ?>" style="--quantity:8">
+                    <?php for ($i = 1; $i <= 8; $i++): ?>
+                        <div class="item" style="--position:<?php echo $i; ?>"
+                             data-image="images/example_<?php echo $i; ?>.jpg">
+                            <img src="images/example_<?php echo $i; ?>.jpg" alt="Example Image <?php echo $i; ?>">
+                            <dialog>
+                                <div class="lightbox-close">
+                                    <div>&#x2715;</div>
+                                </div>
+                                <img src="images/example_<?php echo $i; ?>.jpg" alt="Example Image <?php echo $i; ?>">
+                                <div class="lightbox-content">
+                                    <h3>Image in Dialog</h3>
+                                    <div class="lightbox-text">a little bit of text inside the modal for my fellow
+                                        friends
+                                    </div>
+                                </div>
+                            </dialog>
+                        </div>
+                    <?php endfor; ?>
+                </div>
             </div>
-        <?php endfor; ?>
-    </div>
-    <div class="content">
-        <div class="model">
+            <div class="content">
+                <div class="model">
 
+                </div>
+            </div>
+            <?php if ($showControls): ?>
+                <div class="controls-container">
+                    <div class="prev-container" data-click="prev">
+                        &lsaquo;
+                    </div>
+                    <div class="next-container" data-click="next">
+                        &rsaquo;
+                    </div>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
-
 </div>
 </body>
 </html>
